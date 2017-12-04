08f0833ae0110325efef6d20e886bf8b87254659
Guillaume Peres <guillaumeperes10@gmail.com>
2017-12-04 10:34:47 +0100

    Fix cors

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const UserRole = model.UserRole;
 */
 app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods", "*");
     next();
 });
 
